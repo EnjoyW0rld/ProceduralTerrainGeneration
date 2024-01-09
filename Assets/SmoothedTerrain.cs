043c7484d2176e2ceebe0e5170e04a45b744681f
EnjoyW0rld <denisavatar@hotmail.com>
2024-01-09 16:21:32 +0100

    3D noise test for surface
    Commit before extra 3D nosie will be implemented for cave

--- Assets/SmoothedTerrain.cs
+++ Assets/SmoothedTerrain.cs
@@ -20,12 +20,13 @@ public class SmoothedTerrain : MonoBehaviour
 
         //Generating cave
         int[,,] grid = new int[width, maxDepth + maxElevation, length];
-        CaveLTree.CreateCave(floatGrid, new Vector3(width / 2, maxDepth - 1, length / 2), 30, 10, -1f,2);
-        
+        Add3DNoiseCaves(floatGrid, width, length, maxDepth);
+        CaveLTree.CreateCave(floatGrid, new Vector3(width / 2, maxDepth - 1, length / 2), 30, 10, -1f, 2);
 
         //Surface creation
         GenerateBiomesWithNoiseSmoothed(floatGrid, width, length, maxElevation, maxDepth, ref tex);
-        CaveLTree.DrawLine(floatGrid, new Vector3(width / 2  , maxDepth, length / 2 - 1), new Vector3(width / 2, maxDepth + maxElevation, length / 2));
+        //GenerateBiomes3DSmoothed(floatGrid, width, length, maxElevation, maxDepth, ref tex);
+        CaveLTree.DrawLine(floatGrid, new Vector3(width / 2, maxDepth, length / 2 - 1), new Vector3(width / 2, maxDepth + maxElevation, length / 2));
         Mesh mesh = GetSurcafeMesh();
 
         //combining meshes
@@ -52,9 +53,10 @@ public class SmoothedTerrain : MonoBehaviour
         List<Vector3> vertices = new List<Vector3>();
         List<int> faces = new List<int>();
         MarchingCubes.MarchAlgorithm(floatGrid, vertices, faces, isoLevel,
-            delegate (int x, int y, int z) {
-                if (y < maxDepth - 1) return new Vector3(SCALE,SCALE,SCALE);
-                else return new Vector3(SCALE,1,SCALE);
+            delegate (int x, int y, int z)
+            {
+                if (y < maxDepth - 1) return new Vector3(SCALE, SCALE, SCALE);
+                else return new Vector3(SCALE, 1, SCALE);
             });
         Mesh mesh = new Mesh();
         mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
@@ -71,7 +73,7 @@ public class SmoothedTerrain : MonoBehaviour
     private Mesh GetSurcafeMesh()
     {
         Mesh mesh = new Mesh();
-        MeshData data = MarchingCubes.GetMeshMarchingCubesData(floatGrid, isoLevel,SCALE);
+        MeshData data = MarchingCubes.GetMeshMarchingCubesData(floatGrid, isoLevel, SCALE);
         mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
         mesh.vertices = data.vertices.ToArray();
         mesh.triangles = data.faces.ToArray();
@@ -82,6 +84,7 @@ public class SmoothedTerrain : MonoBehaviour
         mesh.RecalculateNormals();
         return mesh;
     }
+
     private CombineInstance[] CombineMeshes(Mesh mesh1, Mesh mesh2)
     {
         CombineInstance[] meshes = new CombineInstance[2];
@@ -94,10 +97,8 @@ public class SmoothedTerrain : MonoBehaviour
     private static void GenerateBiomesWithNoiseSmoothed(float[,,] grid, int width, int length, int maxElevation, int maxDepth, ref Texture2D tex)
     {
         BiomeManager biome = new BiomeManager(width, length);
-        //int[,,] places = new int[width, maxDepth + maxElevation, length];
         float randVal = System.DateTime.Now.Second;
         float scaleVal = 10f;
-        //Texture2D tex = new Texture2D(width, length); //Generating texture
 
         for (int x = 0; x < width; x++)
         {
@@ -126,11 +127,6 @@ public class SmoothedTerrain : MonoBehaviour
 
                 for (int y = maxDepth; y < maxElevation + maxDepth + ADDCEILING; y++)
                 {
-                    if (y == maxDepth + maxElevation - 1)
-                    {
-                        //grid[x, y, z] = 1;
-                        //continue;
-                    }
                     //(y-maxDepth) (maxElevation - 2)
                     grid[x, y, z] = Mathf.Clamp01(((y - maxDepth) / ((float)maxElevation) + noiseValue) / 2f);
                 }
@@ -140,6 +136,31 @@ public class SmoothedTerrain : MonoBehaviour
         tex = biome.GetTex();
         //tex.Apply();
     }
+    private static void GenerateBiomes3DSmoothed(float[,,] grid, int width, int length, int maxElevation, int maxDepth, ref Texture2D tex)
+    {
+        BiomeManager biome = new BiomeManager(width, length);
+        float randVal = System.DateTime.Now.Second;
+        float scaleVal = 10f;
+
+        for (int x = 0; x < width; x++)
+        {
+            float realX = (x / (float)SCALE) / scaleVal + randVal; //mapped X value
+            for (int z = 0; z < length; z++)
+            {
+                float realZ = (z / (float)SCALE) / scaleVal + randVal; //mapped Z value
+                for (int y = maxDepth; y < maxElevation + maxDepth + ADDCEILING; y++)
+                {
+                    float realY = (y / (float)SCALE) / scaleVal + randVal;
+                    float verticalOffValue = Mathf.LerpUnclamped(0, 1, (y - maxDepth) / ((float)maxElevation));
+                    if (x == 2 && z == 2) print(verticalOffValue);
+                    grid[x, y, z] = Mathf.Clamp01(
+                        verticalOffValue
+                        - Unity.Mathematics.noise.cnoise(new Unity.Mathematics.float3(realX, realY, realZ))
+                        );
+                }
+            }
+        }
+    }
     private static Vector2[] BuildSurfaceUV(List<Vector3> vert, float width, float length)
     {
         Vector2[] uv = new Vector2[vert.Count];
@@ -148,5 +169,24 @@ public class SmoothedTerrain : MonoBehaviour
             uv[i] = new Vector2(vert[i].x / width * SCALE, vert[i].z / length * SCALE);
         }
         return uv;
+    }
+    private static void Add3DNoiseCaves(float[,,] grid, int width,int length, int maxDepth)
+    {
+        float randVal = System.DateTime.Now.Second;
+        float scaleVal = 10f;
+
+        for (int x = 0; x < width; x++)
+        {
+            float realX = (x / (float)SCALE) / scaleVal + randVal; //mapped X value
+            for (int z = 0; z < length; z++)
+            {
+                float realZ = (z / (float)SCALE) / scaleVal + randVal; //mapped Z value
+                for (int y = 0; y < maxDepth; y++)
+                {
+                    float realY = (y / (float)SCALE) / scaleVal + randVal; //mapped Z value
+                    grid[x,y,z] = Unity.Mathematics.noise.cnoise(new Unity.Mathematics.float3(realX, realY, realZ));
+                }
+            }
+        }
     }
 }
